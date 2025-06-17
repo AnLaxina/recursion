f2e9b94c103494442d090614fae431fccf9c133b
AnLaxina <anilovlaxina@gmail.com>
2025-06-16 19:03:08 -0700

    Create new JS file for showing the fibonacci sequence iteratively

---
+++ fibs-iterative.js
@@ -0,0 +1,7 @@
+// Uses iteration over the usual recursion
+
+// Takes a number and returns an array containing that many numbers from the Fib sequence.
+// Ex. numOfElements = 8 would return the array [0, 1, 1, 2, 3, 5, 8, 13]
+function fibs(numOfElements) {
+    
+}
